d051d243ed8aadda5174de7e05f554b3628f713c
Gustave Rousselet <gustave.rousselet@icloud.com>
2016-12-11 19:28:49 +0100

    Checking mechanics.

--- public/Scripts/script.js
+++ public/Scripts/script.js
@@ -36,6 +36,12 @@ function paintPlayers(ctx,canvas, yPos1, yPos2, ballX,ballY){
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
+
+    ctx.beginPath();
+      ctx.arc(ballX, ballY, 2, 0, Math.PI*2);
+      ctx.fillStyle = "#ff0000";
+      ctx.fill();
+      ctx.closePath();
   //console.log(ballX);
   //console.log(ballY);
 
