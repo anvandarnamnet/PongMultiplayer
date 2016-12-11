d51be1ed0d0be9d6e6b1d257e29aabbe0702c703
Gustave Rousselet <gustave.rousselet@icloud.com>
2016-12-11 14:50:12 +0100

    Added functionality for POST requests in backend.

--- Router.js
+++ Router.js
@@ -8,3 +8,11 @@ router.get("/", function (request, response)  {
     var gameState = {"gameID": gameID, "player1Position": 100, "player2Position": 50};
     response.send(gameState);
 });
+
+router.post("/", function (request, response) {
+    var player1Input = request.body.player1Input;
+    var player1Input = request.body.player2Input;
+    console.log(player1Input);
+    console.log(player2Input);
+    response.send("200");
+});
