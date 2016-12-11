d59d943347ec9a23121829485002554f890c7652
Gustave Rousselet <gustave.rousselet@icloud.com>
2016-12-11 15:05:08 +0100

    Update backend for player inputs and updating game state.

--- Router.js
+++ Router.js
@@ -1,6 +1,7 @@
 var app = require("express");
 var router = app.Router();
 var exports = module.exports;
+var gameHandler = require(__dirname + "/GameHandler");
 
 
 router.get("/", function (request, response)  {
@@ -11,10 +12,9 @@ router.get("/", function (request, response)  {
 
 
 router.post("/", function (request, response) {
-    //var player1Input = request;
-    var player2Input = request.body.player1Input;
-    //console.log(player1Input);
-    console.log(player2Input);
+    var player1Input = request.body.player1Input;
+    var player2Input = request.body.player2Input;
+    gameHandler.updateGame(player1Input, player2Input);
     response.send("200");
 });
 
